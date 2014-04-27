290cd4398b39d4d7e979cdb259cf214181c5c31c
saadatnk8 <saadatnawaz4@gmail.com>
2014-04-26 20:16:09 -0400

    Calculating change in rotation matrices

--- MainScript.m
+++ MainScript.m
@@ -1,35 +1,60 @@
-load cube;         %Loading the cube from the data file
-angles = [0 0 0];  %Initial Angles
-targetAngles = [pi/3 pi/2 pi/4]; %[phi theta psi] = angles to be rotated by
-h = 0;       %Reference to the cube object
-T = 10;     %Time
-dt = 0.1;   %Change in time
+function MainScript
 
-figure
-myAxes = axes('xlim', [-2 2], 'ylim', [-2 2], 'zlim', [-2 2]);
-view(3)
-grid on
-axis equal
-hold on
-xlabel('x')
-ylabel('y')
-zlabel('z')
+    data = load('cube');         %Loading the cube from the data file
+    h = 0;       %Reference to the cube object
+    T = 10;     %Time
+    dt = 0.1;   %Change in time
+    nSteps = ceil(T/dt);
 
-for j = 1:dt:T
-    if h ~= 0
-        delete(h);
+    figure
+    myAxes = axes('xlim', [-2 2], 'ylim', [-2 2], 'zlim', [-2 2]);
+    view(3)
+    grid on
+    axis equal
+    hold on
+    xlabel('x')
+    ylabel('y')
+    zlabel('z')
+
+    function a = phi(t)
+        a = t*(pi/3)/10;    %pi/3 is target phi
     end
     
-    rotMat = rotationMatrix(angles);  %Getting rotation matrix from the function
-    angles = angles + targetAngles/(T/dt);  %Updating angles for next loop
-    
-    y = (rotMat*p')'; %Correct order: rotMat*p' but entire thing transposed to make 'patch' work
-    
-    h = patch('Faces',faces,'Vertices',y,'FaceColor','r');
-    set(gca,'CLim',[0 200])          %Plotting cube and its rotations using patch
-    cdata = [110 2 110 2 50 50];
-    set(h,'FaceColor','flat',...
-        'CData',cdata,...
-        'CDataMapping','direct')
-    pause(0.1)
+    function b = theta(t)
+        b = t*(pi/2)/10;    %pi/2 is target theta
+    end
+
+    function c = psi(t)
+        c = t*(pi/4)/10;    %pi/4 is target psi
+    end
+
+    for j = 0:nSteps
+        if h ~= 0
+            delete(h);
+        end
+        
+        t = j*dt;
+        angles = [phi(t) theta(t) psi(t)];
+        rotMat = rotationMatrix(angles);  %Getting rotation matrix from the function
+        
+        %data.p gets points of cube from data file and data.faces gets the
+        %faces
+        
+        y = (rotMat*data.p')'; %Correct order: rotMat*data.p' but entire thing transposed to make 'patch' work
+
+        h = patch('Faces',data.faces,'Vertices',y,'FaceColor','r');
+        set(gca,'CLim',[0 200])          %Plotting cube and its rotations using patch
+        cdata = [110 2 110 2 50 50];
+        set(h,'FaceColor','flat',...
+            'CData',cdata,...
+            'CDataMapping','direct')
+        pause(0.1)
+        
+        changeAngles = [phi((j+1)*dt) theta((j+1)*dt) psi((j+1)*dt)]; %calculating angles for get d(rotMat)/dt
+        changeRotMat = (rotationMatrix(changeAngles)-rotMat)/dt; %calculating d(rotMat)/dt
+        
+        q = rotMat*changeRotMat; %rotMat*d(rotMat)/dt
+        
+    end
+
 end
